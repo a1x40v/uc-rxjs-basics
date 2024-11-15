267e215bca8dcdc00f238ffb939dfa5451817352
a1x40v <a1x40v@gmail.com>
2024-11-15 07:43:59 +0300

    Less-05. Deliver values async with Observables

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
 import { Observable, Observer } from 'rxjs';
 
-const observer: Observer<string> = {
+const observer: Observer<number> = {
   next: (value) => console.log('next', value),
   error: (error) => console.log('error', error),
   complete: () => console.log('complete'),
 };
 
-const observable = new Observable<string>((subscriber) => {
-  subscriber.next('Hello');
-  subscriber.next('World');
-  subscriber.complete();
-  subscriber.next('Ignored value');
+const observable = new Observable<number>((subscriber) => {
+  let count = 0;
+
+  const id = setInterval(() => {
+    subscriber.next(count);
+    subscriber.complete(); // complete after first value
+    count++;
+  }, 1000);
+
+  return () => {
+    // clean up resources
+    console.log('clearing called');
+    clearInterval(id);
+  };
 });
 
+console.log('begin');
 observable.subscribe(observer);
+console.log('end');
