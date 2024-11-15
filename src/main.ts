24576420c45153b4c2a7bcdfe7693409f7981f37
a1x40v <a1x40v@gmail.com>
2024-11-15 07:40:56 +0300

    Less-03. Create Observable

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-// code...
+import { Observable, Observer } from 'rxjs';
+
+const observer: Observer<string> = {
+  next: (value) => console.log('next', value),
+  error: (error) => console.log('error', error),
+  complete: () => console.log('complete'),
+};
+
+const observable = new Observable<string>((subscriber) => {
+  subscriber.next('Hello');
+  subscriber.next('World');
+  subscriber.complete();
+  subscriber.next('Ignored value');
+});
+
+observable.subscribe(observer);
