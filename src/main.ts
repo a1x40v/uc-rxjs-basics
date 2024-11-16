b783122ed32186e919e42549ba462ded057a91cd
a1x40v <a1x40v@gmail.com>
2024-11-16 11:07:57 +0300

    Less-24. take, first

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-import { lab02 } from './labs/lab02';
+import { first, fromEvent, map, of, take } from 'rxjs';
 
-lab02();
+const click$ = fromEvent<PointerEvent>(document, 'click');
+
+click$
+  .pipe(
+    map((event: PointerEvent) => ({
+      x: event.clientX,
+      y: event.clientY,
+    })),
+    // take(1)
+    first(({ y }) => y > 200)
+  )
+  .subscribe({
+    next: console.log,
+    complete: () => console.log('complete'),
+  });
