d397448ae0ab008a000d65cafd83cd99fdd81433
a1x40v <a1x40v@gmail.com>
2024-11-16 19:03:16 +0300

    Less-35. mergeMap

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
-import { debounceTime, fromEvent, mergeMap } from 'rxjs';
+import { fromEvent, interval, map, mergeMap, takeUntil } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
-// elements
-const inputBox = document.getElementById('text-input')!;
+const click$ = fromEvent<MouseEvent>(document, 'click');
+const mousedown$ = fromEvent(document, 'mousedown');
+const mouseup$ = fromEvent(document, 'mouseup');
 
-// streams
-const input$ = fromEvent(inputBox, 'keyup');
+const interval$ = interval(1000);
 
-input$
-  .pipe(
-    debounceTime(1000),
-    mergeMap((event) => {
-      const term = (event.target as HTMLInputElement).value;
-      return ajax.getJSON(`https://api.github.com/users/${term}`);
-    })
+// mousedown$
+//   .pipe(mergeMap(() => interval$.pipe(takeUntil(mouseup$))))
+//   .subscribe(console.log);
+
+const coordinates$ = click$.pipe(
+  map((event) => ({ x: event.clientX, y: event.clientY }))
+);
+
+const coordinatesWithSave$ = coordinates$.pipe(
+  mergeMap((coords) =>
+    ajax.post(
+      `https://run.mocky.io/v3/73f5e08e-d6ba-4fd7-a5de-1c5e4d89b329`,
+      coords
+    )
   )
-  .subscribe(console.log);
+);
+
+coordinatesWithSave$.subscribe((response) => {
+  console.log('Coordinates saved:', response);
+});
